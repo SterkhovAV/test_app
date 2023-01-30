f15e8053ffc6bf1465372dafaab1f634b3737a61
Alexandr <sterkhovav18@gmail.com>
2023-01-30 22:51:34 +0300

    swagger didn't generate Example Value

--- src/main/kotlin/ru/sterkhovav/test_app/controllers/OrdersController.kt
+++ src/main/kotlin/ru/sterkhovav/test_app/controllers/OrdersController.kt
@@ -2,9 +2,12 @@ package ru.sterkhovav.test_app.controllers
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController
 import ru.sterkhovav.test_app.controllers.endpoints.PURCHASE_INFO_REQUEST_PATH
 import ru.sterkhovav.test_app.dto.OrderRequestDto
 import ru.sterkhovav.test_app.service.OrderServiceImpl
+import java.awt.print.Book
+
 
 @Tag(name = "Контроллер обработки заказов")
 @RestController
@@ -25,7 +30,31 @@ class OrdersController(
 ) {
     @Operation(description = "Метод для регистрации заказа")
     @PostMapping(value = ["/new-order"], consumes = [MediaType.APPLICATION_XML_VALUE])
-    fun getPerson(@RequestBody orderRequestDto: OrderRequestDto): ResponseEntity<HttpStatus> {
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200", description = "Successful Operation",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_XML_VALUE, array = (
+                            ArraySchema(
+                                schema = Schema(implementation = OrderRequestDto::class)
+                            ))
+                )]
+            ),
+            ApiResponse(
+                responseCode = "500", description = "Something went wrong",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_XML_VALUE, array = (
+                            ArraySchema(
+                                schema = Schema(implementation = OrderRequestDto::class)
+                            ))
+                )]
+            ),
+        ]
+    )
+    fun getPerson(
+        @RequestBody orderRequestDto: OrderRequestDto
+    ): ResponseEntity<HttpStatus> {
         orderServiceImpl.saveRequestOrder(orderRequestDto)
         return ResponseEntity.ok(HttpStatus.OK)
     }
--- src/main/kotlin/ru/sterkhovav/test_app/dto/OrderRequestDto.kt
+++ src/main/kotlin/ru/sterkhovav/test_app/dto/OrderRequestDto.kt
@@ -1,29 +1,40 @@
 package ru.sterkhovav.test_app.dto
 
-import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Schema
 import java.util.*
-import javax.validation.constraints.Max
-import javax.validation.constraints.Min
 import javax.xml.bind.annotation.XmlRootElement
 
 
 @XmlRootElement
-@Schema(name = "orderRequestDto")
+@Schema(name = "orderRequestDto", description = "Добавление заказа")
 class OrderRequestDto(
-//    @field:Schema(
-//        description = "A year when this car was made",
-//        example = "2021",
-//        type = "int",
-//        minimum = "1900",
-//        maximum = "2500"
-//    )
+    @field:Schema(
+        description = "Имя пользователя", example = "Валерий", type = "string", minLength = 1, maxLength = 50
+    )
     var name: String? = null,
+    @field:Schema(
+        description = "Фамилия пользователя", example = "Петров", type = "string", minLength = 1, maxLength = 50
+    )
     var lastName: String? = null,
+    @field:Schema(
+        description = "Возраст пользователя", example = "25", type = "int", minimum = "1", maximum = "149"
+    )
     var age: Int? = null,
+    @field:Schema(
+        description = "id товара", example = "3", type = "int", minimum = "1", maximum = "5"
+    )
     var purchaseItem: Int? = null,
+    @field:Schema(
+        description = "Количество товара", example = "2", type = "int", minimum = "1"
+    )
     var count: Int? = null,
+    @field:Schema(
+        description = "Сумма заказа", example = "1425", type = "double", minimum = ">0"
+    )
     var amount: Double? = null,
+    @field:Schema(
+        description = "Дата покупки", example = "2021-07-20T18:13:39.49", type = "date"
+    )
     var purchaseDate: Date? = null
 ) {
     //для отладки
